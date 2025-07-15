4a7a4ca2db8a62fe3b82c6ac72ceaebfdb583935
Christopher Vestman <christophervestman@gmail.com>
2025-07-15 11:51:44 +0200

    chore: update vimconfig

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,4 +1,16 @@
 vim.g.lazyvim_check_order = false
 
+vim.opt.autoindent = true
+vim.opt.backspace = { "indent", "eol", "start" }
+vim.opt.expandtab = true
 vim.opt.ignorecase = true
 vim.opt.scrolloff = 8
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+
+-- Highlight trailing whitespace
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = "*",
+	command = [[match ErrorMsg /\s\+$/]],
+})
