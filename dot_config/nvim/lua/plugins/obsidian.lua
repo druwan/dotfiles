e82cd11f7b66aed525af2eb17e58bbf305fb9fc0
Christopher Vestman <christophervestman@gmail.com>
2025-10-06 17:04:41 +0200

    feat(nvim): obsidian.lua

---
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,12 @@
+return {
+	"obsidian-nvim/obsidian.nvim",
+	version = "*",
+	ft = "markdown",
+	---@module 'obsidian'
+	---@type obsidian.config
+	opts = {
+		workspaces = {
+			{ name = "personal", path = vim.fn.expand(os.getenv("NOTES")) },
+		},
+	},
+}
