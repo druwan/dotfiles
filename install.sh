5b9ab67717be19905d3f27e16be0e5930bc34f0d
Christopher Vestman <christophervestman@gmail.com>
2025-11-19 23:48:30 +0100

    fix install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,14 @@
 
 set -euo pipefail
 
+mkdir -p "$HOME/.local/bin"
+
 if ! command -v chezmoi >/dev/null 2>&1; then
   echo "[install.sh] - Installing Chezmoi..."
   sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$HOME/.local/bin"
 fi
 
 echo "[install.sh] - Applying dotfiles"
-chezmoi init --apply "$PWD"
+"$HOME/.local/bin/chezmoi" init --apply --source="$PWD"
 
 exit 0
