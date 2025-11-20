54899d6cadee10550c6c9c4c52d279c2f702aeb5
Christopher Vestman <christophervestman@gmail.com>
2025-11-20 22:15:54 +0100

    debug: install

--- install.sh
+++ install.sh
@@ -13,7 +13,17 @@ if ! command -v chezmoi >/dev/null 2>&1; then
   sh -c "$(curl -fsLS get.chezmoi.io/lb)" -- init --apply git@github.com:druwan/dotfiles.git
 fi
 
-#"$BIN/mise" trust "$HOME/.config/mise/config.toml"
-#"$BIN/mise" install --yes
+if ! command -v mise >/dev/null 2>&1; then
+  echo "[install.sh] - Installing mise..."
+  curl https://mise.run | sh || true
+fi
+
+export PATH="$BIN:$PATH"
+MISE_BIN="$(command -v mise || true)"
+if [ -n "$MISE_BIN" ]; then
+  "$MISE_BIN" trust "$HOME/.config/mise/config.toml" || true
+  "$MISE_BIN" install --yes || true
+fi
+echo "[install.sh] - Done"
 
 exit 0
