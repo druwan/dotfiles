4e42436ecccb6883cb6e6bb5ea406205b5dc7d18
Christopher Vestman <christophervestman@gmail.com>
2025-11-20 20:33:28 +0100

    rm shell

--- install.sh
+++ install.sh
@@ -7,10 +7,6 @@ mkdir -p "$HOME/.local/bin"
 BIN="$HOME/.local/bin"
 DOTFILES_DIR="$HOME/dotfiles"
 
-if command -v zsh >/dev/null; then
-  sudo chsh -s "$(command -v zsh)" "$USER"
-fi
-
 if ! command -v chezmoi >/dev/null 2>&1; then
   echo "[install.sh] - Installing Chezmoi..."
   sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$BIN"
