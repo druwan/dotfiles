2e8e9b2f3e4b7324819ab83707e13355af88e16a
Christopher Vestman <christophervestman@gmail.com>
2025-11-20 21:14:53 +0100

    debug: install.sh

--- install.sh
+++ install.sh
@@ -8,11 +8,6 @@ if command -v zsh >/dev/null; then
   sudo chsh -s "$(command -v zsh)" "$USER"
 fi
 
-if ! command -v mise >/dev/null 2>&1; then
-  echo "Error: mise not found" >&2
-  exit 1
-fi
-
 if ! command -v chezmoi >/dev/null 2>&1; then
   echo "[install.sh] - Installing Chezmoi..."
   sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$BIN"
@@ -21,7 +16,7 @@ fi
 echo "[install.sh] - Applying dotfiles"
 "$BIN/chezmoi" -- init --apply git@github.com:druwan/dotfiles.git
 
-"$BIN/mise" trust "$HOME/.config/mise/config.toml"
-"$BIN/mise" install --yes
+#"$BIN/mise" trust "$HOME/.config/mise/config.toml"
+#"$BIN/mise" install --yes
 
 exit 0
