232b9b9063c9a04960bbbdc9e8fe4eb33a6af1c8
Christopher Vestman <christophervestman@gmail.com>
2025-11-20 20:28:39 +0100

    chore: Update install

--- install.sh
+++ install.sh
@@ -4,12 +4,19 @@ set -euo pipefail
 
 mkdir -p "$HOME/.local/bin"
 
+BIN="$HOME/.local/bin"
+DOTFILES_DIR="$HOME/dotfiles"
+
+if command -v zsh >/dev/null; then
+  sudo chsh -s "$(command -v zsh)" "$USER"
+fi
+
 if ! command -v chezmoi >/dev/null 2>&1; then
   echo "[install.sh] - Installing Chezmoi..."
-  sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$HOME/.local/bin"
+  sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$BIN"
 fi
 
 echo "[install.sh] - Applying dotfiles"
-"$HOME/.local/bin/chezmoi" init --apply --source="$PWD"
+"$BIN/chezmoi" init --apply --source="$DOTFILES_DIR"
 
 exit 0
