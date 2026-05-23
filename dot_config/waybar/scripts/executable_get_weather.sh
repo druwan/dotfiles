abd99efee4d5769b2b47d1dd6e5493bf4620fa8d
Christopher Vestman <christophervestman@gmail.com>
2026-05-23 11:53:48 +0200

    fix: removed +- signs

--- dot_config/waybar/scripts/executable_get_weather.sh
+++ dot_config/waybar/scripts/executable_get_weather.sh
@@ -3,10 +3,10 @@
 for i in {1..5}; do
   text=$(curl -s "https://wttr.in/$1?format=1")
   if [[ $? == 0 ]]; then
-    text=$(echo "$text" | sed -E "s/\s+/ /g")
+    text=$(echo "$text" | sed -E 's/[+-]//g; s/[[:space:]]+/ /g')
     tooltip=$(curl -s "https://wttr.in/$1?format=4")
     if [[ $? == 0 ]]; then
-      tooltip=$(echo "$tooltip" | sed -E "s/\s+/ /g")
+      tooltip=$(echo "$tooltip" | sed -E 's[+-]//g; s/[[:space:]]+/ /g')
       echo "{\"text\":\"$text\", \"tooltip\":\"$tooltip\"}"
       exit
     fi
