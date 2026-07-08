6668ebffa793e1c7f048641db76749ff8a5d026f
Christopher Vestman <christophervestman@gmail.com>
2026-07-08 19:59:39 +0200

    fix(yt-links): add usage, fix loop..

--- dot_local/bin/executable_yt-links.sh
+++ dot_local/bin/executable_yt-links.sh
@@ -1,9 +1,34 @@
 #!/usr/bin/env bash
 
+usage() {
+  cat <<EOF
+Usage: yt-links.sh [OPTIONS] <url> [url2 url3 ...]
+
+Download YouTube videos using yt-dlp.
+
+OPTIONS:
+  -h, --help    Show this help message
+
+EXAMPLES:
+  yt-links.sh https://www.youtube.com/watch?v=NrR_RdRRE2w
+  yt-links.sh https://youtu.be/abc123 https://youtu.be/def456
+EOF
+}
+
 if [ $# -lt 1 ]; then
-  echo -en "Error: Missing yt link(s)"
+  echo "ERROR: Missing yt link(s)"
+  usage
+  exit 1
 fi
 
-links="$*"
+case "$1" in
+-h | --help)
+  usage
+  exit 0
+  ;;
+esac
 
-for link in links; do echo -en "Downloading $link" && yt-dlp --format "bv*[ext=mp4]+ba[ext=m4a]/[ext=mp4" "$link" -o /tmp/; done
+for link in "$@"; do
+  echo -en "Downloading $link"
+  yt-dlp --format "bv*[ext=mp4]+ba[ext=m4a]/[ext=mp4]" "$link" -o /tmp/
+done
